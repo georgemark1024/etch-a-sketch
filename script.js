bc3cea9f320eb0a64ce4cac1fb8be577bfeee8bf
George Mark <144449716+georgemark1024@users.noreply.github.com>
2025-08-07 11:13:09 +0300

    feat: Add sketching and changing grid size
    Allow user to choose a grid size between 1 and 100.
    Add sketching feature that traces mouse movements by changing divs' colors.

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
 const windowWidth = window.innerWidth;
 const windowHeight = window.innerHeight;
-container = document.querySelector("#main-container");
 
 let length = (windowHeight < windowWidth)? windowHeight : windowWidth;
 length = length - 100;
 
-let SIZE_OF_GRID = 16
+container = document.querySelector("#main-container");
+body = document.querySelector("body");
 
-for (let i = 0; i < SIZE_OF_GRID; i++) {
-    divsContainer = document.createElement("div");
-    divsContainer.classList.add("divs-container");
-    for (let j = 0; j < SIZE_OF_GRID; j++) {
-        div = document.createElement("div");
-        div.classList.add("grid-item");
-        div.style.setProperty("flex", `1 0 ${length/SIZE_OF_GRID}px`);
-        div.style.setProperty("height", `${length/SIZE_OF_GRID}px`);
-        divsContainer.appendChild(div);
+
+btn = document.createElement("button");
+btn.textContent = "Change number of squares";
+btn.classList.add("button");
+body.insertBefore(btn, container);
+
+
+btn.addEventListener("click", () => {
+    let squares = 0;
+    while (1) {
+        squares = prompt("Enter number of squares per row and column");
+        if (squares < 1 || squares > 100) {
+            alert("0 < squares <= 100");
+            continue;
+        }
+        break;
     }
-    container.appendChild(divsContainer);
+
+    mainContainer = document.querySelector("#main-container");
+    mainContainer.innerHTML = ""
+    drawGrid(squares);
+})
+
+
+const SIZE_OF_GRID = 16;
+
+function drawGrid(size) {
+    for (let i = 0; i < size; i++) {
+        divsContainer = document.createElement("div");
+        divsContainer.classList.add("divs-container");
+        for (let j = 0; j < size; j++) {
+            div = document.createElement("div");
+            div.classList.add("grid-item");
+            div.style.setProperty("flex", `1 0 ${length/size}px`);
+            div.style.setProperty("height", `${length/size}px`);
+            divsContainer.appendChild(div);
+        }
+        container.appendChild(divsContainer);
+    }
+    addEventListenersToDivs();
 }
 
-divs = document.querySelectorAll(".grid-item");
-divs.forEach(div => {
-    div.addEventListener("mouseenter", () => {
-        div.style.backgroundColor = "red";
-    })
-    div.addEventListener("mouseleave", () => {
-        div.style.backgroundColor = "lightblue";
-    })
-});
+function addEventListenersToDivs() {
+    divs = document.querySelectorAll(".grid-item");
+    divs.forEach(div => {
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = "yellow";
+        })
+        div.addEventListener("mouseleave", () => {
+            div.style.backgroundColor = "black";
+        })
+    });
+}
+
+drawGrid(SIZE_OF_GRID);
